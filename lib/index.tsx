c6cecd17be20f44799a29e5698effb03b7d75a3a
tkmmweb <tkmmweb@qq.com>
2021-11-18 16:02:03 +0800

    安装了react react-dom

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
 
-console.log('object9999999999')
+// console.log('object9999999999')
 const div = document.createElement('div')
-div.innerText = '11321456456456456456456231231'
+div.innerText = '11331'
 document.body.appendChild(div)
+
+console.log(React)
+console.log(ReactDom)
