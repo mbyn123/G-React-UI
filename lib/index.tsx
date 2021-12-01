46a7bf1ea09261a5cb01308087bf88919fd544f1
tkmmweb <tkmmweb@qq.com>
2021-12-01 09:46:18 +0800

    添加新标签

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,5 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Icon from './icon/Icon'
 
+const App = () => {
+    return (
+        <div>
+            <Icon name='wechat' />
+            <Icon name='meituan' />
+            <Icon name='user' />
+            <Icon name='menu' />
+            <Icon name='alipay' />
+        </div>
+    )
+}
 
-ReactDOM.render(<Icon name='wechat'/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
