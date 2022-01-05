738d3803a11fab55a9ed09401b17d72471144c9e
mbyn123 <1832376466@qq.com>
2022-01-05 21:02:13 +0800

    完后Dialog组件基本功能

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,36 +1,83 @@
-import React from "react";
+import React, { ReactElement } from "react";
+import ReactDOM from 'react-dom'
 import './dialog.scss'
 import Icon from "../icon/Icon";
-import {scopedClassMaker} from "../../helpers/classes";
-
-interface Props {
-    visible: boolean,
-}
-
-
+import { scopedClassMaker } from "../../helpers/classes";
 
 const sc = scopedClassMaker('gui-dialog')
 
+interface Props {
+    visible: boolean,
+    onCancel: React.MouseEventHandler,
+    maskClosable?: boolean
+    butttons?: Array<ReactElement>
+}
 
-const Dialog: React.FC<Props> = ({visible, children}) => {
-    return visible ? (
+const Dialog: React.FC<Props> = (props) => {
+
+    const { visible, children, onCancel, maskClosable, butttons } = props
+
+    const onClose: React.MouseEventHandler = (e) => {
+        onCancel(e)
+    }
+
+    const onClickMask: React.MouseEventHandler = (e) => {
+        maskClosable && onClose(e)
+    }
+
+    const dom = visible ? (
         <>
-            <div className={(sc('mask'))}/>
+            <div className={(sc('mask'))} onClick={onClickMask} />
             <div className={sc()}>
-                <div className={sc('close')}>
-                    <Icon name='close'/>
+                <div className={sc('close')} onClick={onClose}>
+                    <Icon name='close' />
                 </div>
-                <header className={sc('close')}>提示</header>
+                <header className={sc('header')}>提示</header>
                 <main className={sc('main')}>
                     {children}
                 </main>
                 <footer className={sc('footer')}>
-                    <button>确定1111</button>
-                    <button>取消</button>
+                    {
+                        butttons ? butttons.map((button, index) => React.cloneElement(button, { key: index })) : (
+                            <>
+                                <button>确定</button>
+                                <button onClick={onClose}>取消</button>
+                            </>
+                        )
+                    }
                 </footer>
             </div>
         </>
     ) : null
+    // 传送门
+    return ReactDOM.createPortal(dom, document.body)
 };
+
+export const alert = (content: string) => {
+    // 声明组件
+    const component = <Dialog visible={true} onCancel={() => { 
+        // 关闭组件,重新渲染一遍组件
+        ReactDOM.render(React.cloneElement(component,{visible:false}),div)
+        // 卸载组件
+        ReactDOM.unmountComponentAtNode(div)
+        // 清楚DOM
+        div.remove()
+    }}>
+        {content}
+    </Dialog>
+
+    // 创建div
+    const div = document.createElement('div')
+
+    // 将div放在body中
+    document.body.append(div)
+
+    // 渲染DOM
+    ReactDOM.render(component, div)
+}
+
+Dialog.defaultProps = {
+    maskClosable: false
+}
 
 export default Dialog
--- lib/dialog/dialogExample.tsx
+++ lib/dialog/dialogExample.tsx
@@ -1,14 +1,22 @@
-import React, {useState} from "react";
-import Dialog from "./dialog";
+import React, { useState } from "react";
+import Dialog, { alert } from "./dialog";
 
 export default function DialogExample() {
     const [visible, setVisible] = useState(false);
     return (
         <>
-            <button onClick={() => setVisible(!visible)}>click</button>
-            <Dialog visible={visible}>
-                 <div>1111</div>
-            </Dialog>
+            <div>
+                <h2>dialog</h2>
+                <button onClick={() => setVisible(!visible)}>click</button>
+                <Dialog visible={visible} onCancel={() => setVisible(false)}>
+                    <div>1111</div>
+                </Dialog>
+            </div>
+            <div>
+                <h2>alert</h2>
+                <button onClick={() => alert('提示')}>alert</button>
+            </div>
+
         </>
     )
 }
