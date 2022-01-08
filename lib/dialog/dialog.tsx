ce3d530094909713407d0ff1648422d5a97a95ee
mbyn123 <1832376466@qq.com>
2022-01-08 18:07:20 +0800

    完成dialog组件

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import ReactDOM from 'react-dom'
 import './dialog.scss'
 import Icon from "../icon/Icon";
@@ -10,15 +10,20 @@ interface Props {
     visible: boolean,
     onCancel: React.MouseEventHandler,
     maskClosable?: boolean
-    butttons?: Array<ReactElement>
+    butttons?: Array<ReactElement>, // 应该设置成footer配置项
+    onOk?: (e: React.MouseEvent<Element, MouseEvent>) => void
 }
 
 const Dialog: React.FC<Props> = (props) => {
 
-    const { visible, children, onCancel, maskClosable, butttons } = props
+    const { visible, children, onCancel, maskClosable, butttons, onOk } = props
 
     const onClose: React.MouseEventHandler = (e) => {
         onCancel(e)
+    }
+
+    const onConfirm: React.MouseEventHandler = (e) => {
+        onOk && onOk(e)
     }
 
     const onClickMask: React.MouseEventHandler = (e) => {
@@ -40,7 +45,7 @@ const Dialog: React.FC<Props> = (props) => {
                     {
                         butttons ? butttons.map((button, index) => React.cloneElement(button, { key: index })) : (
                             <>
-                                <button>确定</button>
+                                <button onClick={onConfirm}>确定</button>
                                 <button onClick={onClose}>取消</button>
                             </>
                         )
@@ -53,16 +58,20 @@ const Dialog: React.FC<Props> = (props) => {
     return ReactDOM.createPortal(dom, document.body)
 };
 
-export const alert = (content: string) => {
-    // 声明组件
-    const component = <Dialog visible={true} onCancel={() => { 
+
+
+const alert = (content: ReactNode) => {
+    const onClose = () => {
         // 关闭组件,重新渲染一遍组件
-        ReactDOM.render(React.cloneElement(component,{visible:false}),div)
+        ReactDOM.render(React.cloneElement(component, { visible: false }), div)
         // 卸载组件
         ReactDOM.unmountComponentAtNode(div)
         // 清楚DOM
         div.remove()
-    }}>
+    }
+
+    // 声明组件
+    const component = <Dialog visible={true} onCancel={onClose} onOk={onClose}>
         {content}
     </Dialog>
 
@@ -76,8 +85,72 @@ export const alert = (content: string) => {
     ReactDOM.render(component, div)
 }
 
+
+const confirm = ({
+    content, yes, no
+}: {
+    content: ReactNode,
+    yes?: () => void,
+    no?: () => void
+}) => {
+
+    return new Promise((resolve, reject) => {
+        const onClose = () => {
+            ReactDOM.render(React.cloneElement(component, { visible: false }), div)
+            ReactDOM.unmountComponentAtNode(div)
+            div.remove()
+        }
+        const onYes = () => {
+            onClose()
+            resolve('yes')
+            yes && yes()
+        }
+
+        const onNo = () => {
+            onClose()
+            reject('no')
+            no && no()
+        }
+
+        const component = <Dialog visible={true} onCancel={onNo} onOk={onYes}>
+            {content}
+        </Dialog>
+
+        const div = document.createElement('div')
+        document.body.append(div)
+        ReactDOM.render(component, div)
+    }).catch(err => err)
+
+}
+
+
+const modal = (content: ReactNode, yes?: () => void) => {
+    const onClose = () => {
+        ReactDOM.render(React.cloneElement(component, { visible: false }), div)
+        ReactDOM.unmountComponentAtNode(div)
+        div.remove()
+    }
+    const onYes = () => {
+        onClose()
+        yes && yes()
+    }
+    const component = <Dialog visible={true} onCancel={onClose} onOk={onYes}>
+        {content}
+    </Dialog>
+
+    const div = document.createElement('div')
+    document.body.append(div)
+    ReactDOM.render(component, div)
+
+    // 将关闭弹窗的方法暴露出去
+    return onClose
+
+}
+
 Dialog.defaultProps = {
     maskClosable: false
 }
+
+export { alert, confirm, modal }
 
 export default Dialog
--- lib/dialog/dialogExample.tsx
+++ lib/dialog/dialogExample.tsx
@@ -1,8 +1,32 @@
 import React, { useState } from "react";
-import Dialog, { alert } from "./dialog";
+import Dialog, { alert, confirm, modal } from "./dialog";
 
 export default function DialogExample() {
     const [visible, setVisible] = useState(false);
+
+    const openModal = () => {
+        // 接受关闭弹窗的方法
+        const close = modal(<div>
+            '11111'
+            {/* 可以在内部关闭弹窗 */}
+            <button onClick={() => close()}>关闭</button>
+        </div>, () => {
+            console.log('你点击了确认')
+        })
+    }
+
+    const x = () => {
+        confirm({
+            content: '1111',
+            yes(){
+                console.log(111)
+            },
+            no(){
+                console.log(222)
+            }
+        })
+
+    }
     return (
         <>
             <div>
@@ -15,6 +39,14 @@ export default function DialogExample() {
             <div>
                 <h2>alert</h2>
                 <button onClick={() => alert('提示')}>alert</button>
+            </div>
+            <div>
+                <h2>confirm</h2>
+                <button onClick={x}>confirm</button>
+            </div>
+            <div>
+                <h2>confirm</h2>
+                <button onClick={openModal}>modal</button>
             </div>
 
         </>
